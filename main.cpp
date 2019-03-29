0b5cc860703ae8869ddf3420336747bc4b6dea49
sergeyzaytsevkaliningrad <37252069+sergeyzaytsevkaliningrad@users.noreply.github.com>
2019-03-29 16:34:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 //
 //  main.cpp
-//  курсовая
+//  1.2 курсовая
 //
-//  Created by Сергей Зайцев on 23/03/2019.
+//  Created by Сергей Зайцев on 29/03/2019.
 //  Copyright © 2019 Сергей Зайцев. All rights reserved.
 //
 
 #include <iostream>
 
 using namespace std;
-
 
 int perevod(int dighit){
     int i;
@@ -40,48 +39,108 @@ int perevod(int dighit){
     return dighit;
 }
 
-
 int main() {
-    int n;
-    int a;
-    cout << "inter your number"<< endl;
-    scanf("%d",&a);
+    int a,b,c,d,e,r ;
+    
+    cout << " you will have to inter your first number step by step\n ";
+    cout << "*0000\n";
+    cin >> a;
+    assert(a<10);
     perevod(a);
     
-    int b;
-    cout << "inter your sign\n";
-    cout << "1=-\n2=+\n3=*\n4=/\n";
-    cin  >> b;
+    cout <<"0*000\n";
+    cin >> b;
+    assert(b<10);
+    perevod(b);
     
-    int c ;
-    cout << "inter your second number\n";
-    scanf("%d",&c);
+    cout << "00*00\n";
+    cin >> c;
+    assert(c<10);
     perevod(c);
     
-    if ( b == isalpha(b)){
+    cout << "000*0\n";
+    cin >> d ;
+    assert(d<10);
+    perevod(d);
+    
+    cout <<"0000*\n";
+    cin >> e;
+    assert(e<10);
+    perevod(e);
+    
+    r = a*10000 + b*1000 + c*100 + d*10 + e;
+    
+    cout << a*10000 + b*1000 + c*100 + d*10 + e<< endl;
+    cout << perevod(a) + perevod(b) + perevod(c) + perevod(d) + perevod(e);
+    
+    
+    int z,x,l,v,m,n,p;
+    
+    cout << " you will have to inter your second number step by step\n ";
+    cout << "*0000\n";
+    cin >> z;
+    assert(z<10);
+    perevod(z);
+    
+    cout <<"0*000\n";
+    cin >> x;
+    assert(x<10);
+    perevod(x);
+    
+    cout << "00*00\n";
+    cin >> l;
+    assert(l<10);
+    perevod(l);
+    
+    cout << "000*0\n";
+    cin >> v ;
+    assert(v<10);
+    perevod(v);
+    
+    cout <<"0000*\n";
+    cin >> m;
+    assert(m<10);
+    perevod(m);
+    
+    p = z*10000 + x*1000 + l*100 + v*10 + l;
+    
+    cout << z*10000 + x*1000 + l*100 + v*10 + l<< endl;
+    cout << perevod(z) + perevod(x) + perevod(l) + perevod(v) + perevod(m);
+    
+    int g;
+    
+    cout <<"now inter number of sign\n";
+    cout << "1=-\n2=+\n3=*\n4=/\n";
+    cin >> g;
+    
+    if ( g == isalpha(g)){
         cout<<"wtf";
     }
-    switch (b) {
+    switch (g) {
         case 1:
-            n = a - c;
+            n = r - p;
             perevod(n);
+            return n;
             break;
             
         case 2:
-            n = a + c;
+            n = r + p;
             perevod(n);
+            return n;
             break;
         case 3:
-            n = a * c;
+            n = r * p;
             perevod(n);
+            return n;
             break;
         case 4:
-            n=a/c;
+            n=r/p;
             perevod(n);
+            return n;
             break;
     }
     
     
     
-    return 0;
 }
+
